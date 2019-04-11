9c09b96198d56404f7ad25fd9b4c291d2535f548
phuclehuu <phuc.le@tech.reapra.sg>
2019-04-11 11:15:17 +0700

    Implement guide test code

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -2,4 +2,5 @@ class Guide < ApplicationRecord
 	has_and_belongs_to_many :languages
 	has_and_belongs_to_many :activities
 	has_many :reviews
+	validates :email, presence: true, uniqueness: true
 end
