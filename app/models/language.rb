ae10a9cff7c5739fbea162c73435c81e7f07eb13
phuclehuu <phuc.le@tech.reapra.sg>
2019-04-11 15:10:44 +0700

    Implement language test code

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,4 @@
 class Language < ApplicationRecord
 	has_and_belongs_to_many :guides
+	validates :code, uniqueness: true, presence: true
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+  before(:each) do
+  	@language = create(:language)
+  end
+
+  it 'is valid with valid attributes' do
+  	expect(@language).to be_valid
+  end
+
+  it 'has a unique code' do
+  	language2 = build(:language, code: @language.code)
+  	expect(language2).to_not be_valid
+  end
+
+  it 'is not valid without code' do
+  	language2 = build(:language, code: nil)
+  	expect(language2).to_not be_valid
+  end
+
 end
