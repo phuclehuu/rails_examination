303bfaff7777f37d236c1ab81bfe5bce3b3cafe5
phuclehuu <phuc.le@tech.reapra.sg>
2019-04-11 11:22:14 +0700

    Implement activity test code

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+  	@activity = create(:activity)
+  end
+
+  it 'is valid with valid attributes' do
+  	expect(@activity).to be_valid
+  end
+
+  it 'has a unique name' do
+  	activity2 = build(:activity, name: @activity.name)
+  	expect(activity2).to_not be_valid
+  end
+
+  it 'is not valid without name' do
+  	activity2 = build(:activity, name: nil)
+  	expect(activity2).to_not be_valid
+  end
+
 end
