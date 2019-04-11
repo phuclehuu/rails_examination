ebcd33ade2c27af7fcd4dea5d20c3ba23c5bdbbc
phuclehuu <phuc.le@tech.reapra.sg>
2019-04-11 11:12:15 +0700

    Implement guide test code

--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Guide, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+  	@guide = create(:guide)
+  end
+
+  it 'is valid with valid attributes' do
+  	expect(@guide).to be_valid
+  end
+
+  it 'has a unique email' do
+  	guide2 = build(:guide, email: @guide.email)
+  	expect(guide2).to_not be_valid
+  end
+
+  it 'is not valid without an email' do
+  	guide2 = build(:guide, email: nil)
+  	expect(guide2).to_not be_valid
+  end
+  
 end
