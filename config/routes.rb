a42b9d7c017108b8415a6d93e70735d12903ada7
phuclehuu <phuc.le@tech.reapra.sg>
2019-04-12 18:06:34 +0700

    Implement reviews page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :items
+  	resources :guides do
+  		resources :reviews, shallow: true
+  	end
 	root to: 'dashboard#index'
 	get :search, controller: :main
 	get :languages_no_one_speaks, controller: :main
