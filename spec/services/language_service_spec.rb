d3ce8718121126d0eaa5ecbb89bee8dd78648167
phuclehuu <phuc.le@tech.reapra.sg>
2019-04-12 14:05:24 +0700

    Update unit test for search languages no one speaks

--- spec/services/language_service_spec.rb
+++ spec/services/language_service_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 describe LanguageService do
   describe "#search" do
   	it "language no one speaks" do
-  		language_no_one_speaks = Language.create(code: 'ru')
+  		language_no_one_speaks = [Language.create(code: 'ru')]
   		language = Language.create(code: 'vn')
   		Guide.create(email: 'john@example.com', language_ids:[language.id])
         searchResults = LanguageService.new.get_unused_languages
-        expect(searchResults).to eq [language_no_one_speaks]
+        expect(searchResults).to eq language_no_one_speaks
     end
   end
 end
